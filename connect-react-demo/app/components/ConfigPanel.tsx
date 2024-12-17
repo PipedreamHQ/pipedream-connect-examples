250b8eea50b909355343de757af6cfd39b470a7f
Jason Endo <jason@pipedream.com>
2024-12-17 13:28:02 -0800

    move webhookurl up and add (required)

--- connect-react-demo/app/components/ConfigPanel.tsx
+++ connect-react-demo/app/components/ConfigPanel.tsx
@@ -240,7 +240,23 @@ export const ConfigPanel = () => {
           readOnly
         />
       </PropertyItem>
-
+      {selectedComponentType === "trigger" && (
+        <PropertyItem
+          name="webhookUrl (required)"
+          type="string"
+          description="Webhook for trigger"
+          required={true}
+        >
+          <input
+            value={webhookUrl}
+            onChange={(e) => {
+              setWebhookUrl(e.target.value)
+            }}
+            placeholder="Enter a webhook URL to receive emitted events"
+            className="w-full px-3 py-1.5 text-sm font-mono border rounded bg-zinc-50/50"
+          />
+        </PropertyItem>
+      )}
       <PropertyItem
         name="componentKey"
         type="string"
@@ -269,23 +285,6 @@ export const ConfigPanel = () => {
           />
         </div>
       </PropertyItem>
-      {selectedComponentType === "trigger" && (
-        <PropertyItem
-          name="webhookUrl"
-          type="string"
-          description="Webhook for trigger"
-          required={true}
-        >
-          <input
-            value={webhookUrl}
-            onChange={(e) => {
-              setWebhookUrl(e.target.value)
-            }}
-            placeholder="Enter a webhook URL to receive emitted events"
-            className="w-full px-3 py-1.5 text-sm font-mono border rounded bg-zinc-50/50"
-          />
-        </PropertyItem>
-      )}
       <PropertyItem
         name="hideOptionalProps"
         type="boolean"
