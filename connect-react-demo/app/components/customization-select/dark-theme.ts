5d4a5e2ce961d67feeaff652cb3df5106cf3d6d0
adolfo-pd <adolfo@pipedream.com>
2024-12-17 11:21:34 -0800

    feat: DJ-2767 Add a spinner to submit button

--- connect-react-demo/app/components/customization-select/dark-theme.ts
+++ connect-react-demo/app/components/customization-select/dark-theme.ts
@@ -6,11 +6,17 @@ const customization: CustomizationConfig = {
       ...base,
       backgroundColor: theme.colors.neutral20,
     }),
-    controlSubmit: (base, { theme }) => ({
-      ...base,
-      color: theme.colors.neutral90,
-      backgroundColor: theme.colors.primary,
-    }),
+    controlSubmit: (base, { form, theme }) => {
+      const spinner = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 100 100"><circle fill="none" stroke="#ffffff" stroke-width="8" cx="50" cy="50" r="44" style="opacity:0.5;"/><circle fill="none" stroke="#cccccc" stroke-width="8" cx="50" cy="50" r="44" stroke-dasharray="172" stroke-dashoffset="86.4" transform="rotate(-90.0001 50 50)"><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50"/></circle></svg>`
+      return {
+        ...base,
+        color: theme.colors.neutral90,
+        backgroundColor: theme.colors.primary,
+        backgroundImage: form.submitting ? `url('data:image/svg+xml;base64,${btoa(spinner)}')` : "unset",
+        backgroundPosition: "right 15% top 50%",
+        backgroundRepeat: "no-repeat",
+      }
+    },
     controlSelect: {
       control: (base, { theme }) => ({
         ...base,
